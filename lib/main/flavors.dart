72f6d5b790f40aa935a14b1eadd2ee598fed2405
Siddharth Dave <siddharthdave164@gmail.com>
2022-10-19 12:51:29 +0530

    splash screen updated

--- lib/main/flavors.dart
+++ lib/main/flavors.dart
@@ -1,3 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+
+import '../core/configs/firebase_options/firebase_options_dev.dart' as dev;
+import '../core/configs/firebase_options/firebase_options_prod.dart' as prod;
+import '../core/configs/firebase_options/firebase_options_staging.dart'
+    as staging;
+
 enum Flavor {
   dev,
   staging,
@@ -19,6 +26,19 @@ class F {
         return 'MassyStores';
       default:
         return 'title';
+    }
+  }
+
+  static FirebaseOptions get firebaseOption {
+    switch (appFlavor) {
+      case Flavor.dev:
+        return dev.DefaultFirebaseOptions.android;
+      case Flavor.staging:
+        return staging.DefaultFirebaseOptions.android;
+      case Flavor.prod:
+        return prod.DefaultFirebaseOptions.android;
+      default:
+        return dev.DefaultFirebaseOptions.android;
     }
   }
 }
