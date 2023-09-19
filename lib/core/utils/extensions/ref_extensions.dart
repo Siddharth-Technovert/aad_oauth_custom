5b3c4ab38fa56d6a4e0104fa6cca8cc866a8b491
Siddharth Dave <siddharth.d@technovert.com>
2023-09-19 14:39:25 +0530

    added issue url

--- lib/core/utils/extensions/ref_extensions.dart
+++ lib/core/utils/extensions/ref_extensions.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 extension CancelTokenX on AutoDisposeRef {
+  //?Cancel Token issue on dispose https://github.com/cfug/dio/issues/1953
   CancelToken cancelToken() {
     final cancelToken = CancelToken();
     onDispose(cancelToken.cancel);
