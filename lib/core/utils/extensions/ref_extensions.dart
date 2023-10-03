139fc8793673fcc77b352a800c3f0c651f064aa1
Siddharth Dave <siddharth.d@technovert.com>
2023-10-03 16:06:47 +0530

    created cache for and debounce function with ref lifecycle

--- lib/core/utils/extensions/ref_extensions.dart
+++ lib/core/utils/extensions/ref_extensions.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,20 +8,39 @@ extension CancelTokenX on AutoDisposeRef {
   CancelToken cancelToken() {
     final cancelToken = CancelToken();
     onDispose(cancelToken.cancel);
-    // Timer? timer;
-    // final link = keepAlive();
-    // onDispose(() {
-    //   cancelToken.cancel();
-    //   // timer?.cancel();
-    // });
-    // onCancel(() {
-    //   timer = Timer(const Duration(seconds: 5), () {
-    //     link.close();
-    //   });
-    // });
-    // onResume(() {
-    //   timer?.cancel();
-    // });
+
     return cancelToken;
+  }
+
+  KeepAliveLink cacheFor({Duration duration = const Duration(seconds: 2)}) {
+    Timer? timer;
+    final link = keepAlive();
+    onDispose(() {
+      // cancelToken.cancel();
+      timer?.cancel();
+    });
+    onCancel(() {
+      timer = Timer(duration, link.close);
+    });
+    onResume(() {
+      timer?.cancel();
+    });
+    return link;
+  }
+
+  Future<void> debounce({Duration duration = const Duration(seconds: 2)}) {
+    final completer = Completer<void>();
+    final Timer timer = Timer(duration, () {
+      if (!completer.isCompleted) {
+        completer.complete();
+      }
+    });
+    onDispose(() {
+      timer.cancel();
+      if (!completer.isCompleted) {
+        completer.completeError(StateError("Cancelled"));
+      }
+    });
+    return completer.future;
   }
 }
